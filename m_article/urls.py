3e47d189382e6f1b30b6a550e7c84b66b4c85be5
ali <dadsetan.ali@gmail.com>
2015-06-09 09:27:43 +0430

    minor

--- m_article/urls.py
+++ m_article/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', views.ArticleListView.as_view()),
     url(r'^tag/(?P<tag_name>[^/]*)/$', views.ArticleListView.as_view(), name="tag_view"),
     url(r'^category/(?P<category_name>.*)/$', views.ArticleListViewByCategory.as_view(), name="article_view_by_category"),
-    url(r'^(?P<category_name>[^/]*)/(?P<article_title>[^/]*)/$', views.ArticleView.as_view(), name="article_view"),
+    url(r'^(?P<category_name>.*)/(?P<article_title>[^/]*)/$', views.ArticleView.as_view(), name="article_view"),
 
     # url(r'^$', views.test_page)
     )
